1387b5dcc625eb9dd0766b1c0611a7a207929914
Fengyi Li <fengyi.li18@imperial.ac.uk>
2022-07-25 14:46:12 +0800

    feat(funcs): Convert the tif image into png

--- funcs.py
+++ funcs.py
@@ -1,8 +1,10 @@
+import glob
 import os
 from datetime import datetime
 
 import cv2
 import numpy as onp
+from PIL import Image
 
 
 def create_dir(path):
@@ -25,16 +27,16 @@ def filenames_generator(n, n_local, l_vid_name, r_vid_name, args):
 
     # Write the image names into the txt file for further training
     for i in indexes_list:
-        filenames_txt.write(path_generator("left", l_vid_name, f"img_{i}_left.png"))
+        filenames_txt.write(os.path.join("left", l_vid_name, f"img_{i}_left.png"))
         filenames_txt.write(" ")
-        filenames_txt.write(path_generator("right", r_vid_name, f"img_{i}_right.png"))
+        filenames_txt.write(os.path.join("right", r_vid_name, f"img_{i}_right.png"))
         filenames_txt.write("\n")
 
     filenames_txt.close()
 
 
 def filenames_generator_mono(n, n_local, l_vid_name, r_vid_name, args):
-    def txt_write(filenames_txt, i_img, l_vid_name, position):
+    def txt_write_real(filenames_txt, i_img, l_vid_name, position):
         filenames_txt.write(os.path.join(position, l_vid_name))
         filenames_txt.write(" ")
         filenames_txt.write(str(i_img))
@@ -61,14 +63,32 @@ def filenames_generator_mono(n, n_local, l_vid_name, r_vid_name, args):
     # Write the image names into the txt file for further training
     for i in indexes_list:
         if args.stereo or not should_be_removed(i, n, n_local):
-            txt_write(filenames_txt, i, l_vid_name, "left")
-            txt_write(filenames_txt, i, r_vid_name, "right")
+            txt_write_real(filenames_txt, i, l_vid_name, "left")
+            txt_write_real(filenames_txt, i, r_vid_name, "right")
 
     filenames_txt.close()
 
 
-def path_generator(direction, vid_name, img_name):
-    return os.path.join(direction, vid_name, img_name)
+def filename_generator_rendered(n, n_local, output_path, file_name):
+    index_list = onp.arange(n - n_local + 1, n + 1)
+    onp.random.shuffle(index_list)
+
+    filenames_txt = open(os.path.join(output_path, file_name + ".txt"), "w")
+
+    for i in index_list:
+        txt_write_rendered(filenames_txt, i, "Left")
+        txt_write_rendered(filenames_txt, i, "Right")
+
+    filenames_txt.close()
+
+
+def txt_write_rendered(filenames_txt, i_img, position):
+    filenames_txt.write(position)
+    filenames_txt.write(" ")
+    filenames_txt.write(str(i_img))
+    filenames_txt.write(" ")
+    filenames_txt.write(f"{position[0]}".lower())
+    filenames_txt.write("\n")
 
 
 def remove_timestamp(img, direction, args):
@@ -95,3 +115,26 @@ def remove_timestamp(img, direction, args):
     # save the result
     return masked_image
 
+
+def tif_to_png(img_dir, output_dir):
+    create_dir(output_dir)
+    i = 0
+    for dir_path in glob.glob(os.path.join(img_dir, "*/")):
+        new_output_dir = os.path.join(output_dir, dir_path.split("/")[-2])
+        create_dir(new_output_dir)
+        for img_path in sorted(glob.glob(os.path.join(dir_path, "*.tif"))):
+            i += 1
+            img_name = img_path.split("/")[-1].split(".")
+            img_name[-1] = "PNG"
+            img_name = ".".join(img_name)
+            outfile = os.path.join(new_output_dir, img_name)
+            im = Image.open(img_path)
+            im.thumbnail(im.size)
+            im.save(outfile, "png", quality=100)
+
+            if i % 100 == 0:
+                print(f"-> Convert {i} images ...")
+
+
+if __name__ == "__main__":
+    filename_generator_rendered(1000, 1000, "", "rendered_train_data")
